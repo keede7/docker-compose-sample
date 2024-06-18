e2a82f7be57e8ba5aed252ac414c7fade6f74ab5
keede <wesaq11@gmail.com>
2024-06-18 11:30:45 +0900

    명칭 변경

--- src/main/java/com/example/dockercomposesample/TestController.java
+++ src/main/java/com/example/dockercomposesample/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+//     1. Registry 이미지 올라가는지,
+//            2. pull 되는지,
 
     @GetMapping("/")
     public String main() {
